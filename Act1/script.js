3bdce1d2f2908511b3fce820363942fad4018f92
Thalia <thalia.soler.7e7@itb.cat>
2024-09-17 19:17:16 +0200

    add structure

---
+++ Act1/script.js
@@ -0,0 +1,30 @@
+function compararNumeros(a,b){
+    if(a>b){
+        return a
+    }
+    return b
+}
+
+function suma1a100(){
+    for(i=1; i<100)
+}
+
+function pedraPaper(){
+
+}
+
+function esParell(){
+
+}
+
+function revertirCadena(cadena){
+
+}
+
+function factorial(n){
+
+}
+
+function filtrarPositius(array){
+
+}
