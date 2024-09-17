e3d111bc09796348c44db7562e0dd79653c1e1c1
Thalia <thalia.soler.7e7@itb.cat>
2024-09-17 19:29:05 +0200

    first 3 exercicies done

--- Act1/script.js
+++ Act1/script.js
@@ -6,11 +6,26 @@ function compararNumeros(a,b){
 }
 
 function suma1a100(){
-    for(i=1; i<100)
+    let suma
+    for(i=1; i<100; i++){
+        suma=suma+i
+    }
+    console.log(suma)
 }
 
 function pedraPaper(){
-
+    for(i=1; i<20; i++){
+        switch(i){
+            case i % 3 ==0:
+            console.log("Pedra")
+            case i % 5 ==0:
+            console.log("Paper")
+            case i % 3 ==0 && i % 5 ==0:
+            console.log("Tisores")
+            default:
+            console.log(i)
+        }
+    }
 }
 
 function esParell(){
