774d5a70b80b733f7a53871cc040f3424e9f5b0c
Thalia <thalia.soler.7e7@itb.cat>
2024-09-17 20:03:20 +0200

    falta acabar actividad 6 y 7

--- Act1/script.js
+++ Act1/script.js
@@ -1,13 +1,13 @@
 function compararNumeros(a,b){
     if(a>b){
-        return a
+        console.log(a)
     }
-    return b
+    console.log(b)
 }
 
 function suma1a100(){
-    let suma
-    for(i=1; i<100; i++){
+    let suma=0
+    for(i=0; i<100; i++){
         suma=suma+i
     }
     console.log(suma)
@@ -28,18 +28,33 @@ function pedraPaper(){
     }
 }
 
-function esParell(){
-
+function esParell(num){
+    if(num %2==0){
+        console.log("True")
+    }else
+    console.log("False")
 }
 
 function revertirCadena(cadena){
-
+    let cadenaFinal=cadena.split('').reverse().join('')
+    console.log(cadenaFinal)
 }
 
 function factorial(n){
-
+    if(n>0){
+        console.log(n)
+    }else {
+        return factorial(n*(n-1))
+    }
 }
 
 function filtrarPositius(array){
-
+    for(i=0; i<array.length; i++){
+        if(array[i]<0){
+            array.pop(i)
+        }
+        return array
+    }
+    console.log(array)
+    
 }
