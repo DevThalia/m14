5612a1d02cf22aa39aec8abb39041fd00c084659
Thalia <thalia.soler.7e7@itb.cat>
2024-10-01 19:08:00 +0200

    parte 2 hecha

--- AEA_1.04/script.js
+++ AEA_1.04/script.js
@@ -1,6 +1,6 @@
 function ejercicio1() {
 
-    const estudiant = {
+    let estudiant = {
         nom: 'Joan',
         edat: 20,
         notes: {
@@ -18,11 +18,60 @@ function ejercicio1() {
 }
 
 function ejercicio2() {
-    
-    const coordenades = [12, 5, 8];
+
+    let coordenades = [12, 5, 8];
 
     // Desestructura l'array aquí
-    let [num1,num2,num3]=coordenades
+    let [num1, num2, num3] = coordenades
+
     // Imprimeix les coordenades: "Les coordenades són x=12, y=5, z=8."
-    console.log(`Les coordenades són ${num1}, ${num2}, ${num3}`);
+    console.log(`Les coordenades són ${num1}, ${num2}, ${num3}`)
+}
+
+function ejercicio3() {
+
+    let preus = [100, 200, 300, 400];
+
+    // Aplica el mètode map per incrementar els preus en un 10%
+    let newArray = preus.map(function (num) {
+        return (num * 0.1) + num;
+    })
+
+    // Imprimeix el nou array de preus
+    console.log(newArray)
+}
+
+function ejercicio4() {
+
+    let edats = [16, 21, 18, 24, 12, 30];
+
+    // Utilitza el mètode filter per obtenir les edats majors de 18
+    let newArray = edats.filter(function (num) {
+        return num > 18
+    })
+    // Imprimeix el nou array
+    console.log(newArray)
+}
+
+function ejercicio5() {
+    let notes = [7, 9, 5, 10, 8];
+
+    // Utilitza reduce per calcular la mitjana
+
+    let newArray = notes.reduce(function(suma,num){
+        return suma+num
+    })
+
+    let media=newArray/notes.length
+
+    // Imprimeix el resultat
+    console.log(media)
+}
+
+function ejercicio6(){
+    
+}
+
+function ejercicio7(){
+
 }
